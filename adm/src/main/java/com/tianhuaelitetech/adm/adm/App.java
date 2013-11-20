81b2a07a1eebddf55a48a5cbe185c38885325a46
arehero <834815821@qq.com>
2013-11-20 14:08:25 +0800

    fisrt commit

---
+++ adm/src/main/java/com/tianhuaelitetech/adm/adm/App.java
@@ -0,0 +1,13 @@
+package com.tianhuaelitetech.adm.adm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
